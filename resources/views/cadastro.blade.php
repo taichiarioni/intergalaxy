b5929faea96172d041911f13223d1ff98c612f66
Taianne Fernandes <taichiarioni@gmail.com>
2021-05-27 16:15:45 -0300

    routes

--- resources/views/cadastro.blade.php
+++ resources/views/cadastro.blade.php
@@ -3,10 +3,17 @@
 @section('content')
     <div class="container">
         <div class="card-header">{{ __('Cadastro de Prestadores') }}
+
             <form action="">
+            @csrf
                 <label for="nome">
                     <span><br>Nome</span>
                     <input type="text" id="nome" name="nome">
+                </label>
+
+                <label for="sobrenome">
+                    <span><br>Sobrenome</span>
+                    <input type="text" id="sobrenome" name="sobrenome">
                 </label>
 
                 <label for="email">
@@ -18,13 +25,12 @@
                     <span>Telefone</span>
                     <input type="text" id="telefone" name="telefone">
                 </label>
-
+            <div>
                 <label for="nasc">
                     <span>Data de Nascimento</span>
                     <input type="date" id="nasc" name="nasc">
                 </label>
-            <div>
-                <label for="nasc">
+                <label for="area">
                     <span>Área</span>
                     <input type="text" id="area" name="area">
                     <span>Cargo</span>
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Site\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -31,5 +22,10 @@ Route::get('/relatorios', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// Route::namespace('Site')->group( function() {
+//     Route::get('/', [App\Http\Controllers\HomeController::class]);
+// });
 
